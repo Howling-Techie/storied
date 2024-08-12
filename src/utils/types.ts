1e8483e02ba40d8f2ab7af0232891f936e6666b2
Howling Techie <howling.techie@gmail.com>
2024-08-12 13:53:11 +0100

    Converted types to be inheriting from a base entity type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,56 +1,58 @@
-export type Project = {
+export type BaseEntity = {
+    project_id: number,
     id: number,
     name: string,
     description: string,
-    icon: string,
+    icon: string | null,
+    text: string | null,
+    filename: string,
+    status: Status,
     creation_date: Date,
     last_modified: Date,
-    tags: string[]
+    tags: Tag[];
 }
+
+export type Tag = {
+    id: number,
+    icon: string,
+    name: string,
+}
+
+export type Status = "concept" | "outlined" | "writing" | "editing" | "finished";
+export type EntityType = "Location" | "Character" | "Story" | "Chapter" | "Scene" | "Event";
+
+export type Project = Omit<BaseEntity, "project_id">
 
 export type Location = {
-    id: number,
-    project_id: number,
-    name: string,
-    description: string,
-    filename: string,
-    text: string | null,
-    icon: string
-}
+    parent_location_id: number | null
+} & BaseEntity
 
 export type Character = {
-    id: number,
-    project_id: number,
-    name: string,
-    filename: string,
     role: string,
-    description: string,
-    text: string,
-    icon: string,
     traits: { icon: string, trait: string }[],
-    relationships: { character: Character, type: string }[]
-}
+    relationships: { character_id: number, type: string }[]
+} & BaseEntity
+
+export type Story = {
+    project_id: number,
+} & BaseEntity
 
 export type Chapter = {
-    id: number,
-    project_id: number,
-    name: string,
-    filename: string,
+    story_id: number,
     position: number,
-    description: string,
-    text: string
-}
+} & BaseEntity
 
 export type Scene = {
-    id: number,
-    project_id: number,
     chapter_id: number,
-    name: string,
-    filename: string,
     position: number,
-    description: string,
-    text: string,
-    status: "concept" | "outlined" | "writing" | "editing" | "finished",
+    status: Status,
     locations: Location[],
     characters: Character[],
-}
+} & BaseEntity
+
+export type Event = {
+    previous_event_ids: number[],
+    next_event_ids: number[],
+    locations: Location[],
+    characters: Character[],
+} & BaseEntity
